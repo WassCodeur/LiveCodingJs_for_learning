27493d7bba3d61e0718f325d32c2bea308a40b58
WassCodeur <wachioubouraima56@gmail.com>
2022-06-18 07:04:29 +0000

    my first live coding

---
+++ index.js
@@ -0,0 +1,22 @@
+// console.log('hello')
+// let h1 = document.getElementsByTagName('h1')
+// // h1.innerHTML = 'Bonjour man'
+
+// console.log(h1)
+// let firstH1 = h1[0]
+
+// firstH1.textContent = 'Bonsoir Wassiou'
+// let firstH2 = h1[1]
+
+// console.log('text', firstH2.textContent)
+// console.log('innerhtml', firstH2.innerHTML)
+let headers = document.getElementsByTagName('h1')
+
+// for (const elt of headers) {
+//   let span = elt.getElementsByTagName('span')[0]
+//   console.log(span)
+// }
+let he =document.getElementById('head-1')
+console.log(he)
+
+  
